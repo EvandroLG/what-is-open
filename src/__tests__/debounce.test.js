604bcdb80bef89240aee3aa52fcd7f60006cea98
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-02-11 20:53:24 +0000

    add unit tests to debounce component

---
+++ src/__tests__/debounce.test.js
@@ -0,0 +1,29 @@
+import debounce from '../debounce';
+
+jest.useFakeTimers();
+
+describe('debounce', () => {
+    it('should call setTimeout using the timer passed as parameter', () => {
+        const callback = jest.fn();
+        const timer = 500;
+
+        debounce(callback, timer)();
+
+        expect(setTimeout).toHaveBeenCalledTimes(1);
+        expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), timer);
+    });
+
+    it('should execute callback passing the expected parameter', () => {
+        const callback = jest.fn();
+        const timer = 500;
+        const args = 'a';
+
+        debounce(callback, timer)(args);
+
+        expect(callback).not.toBeCalled();
+        jest.runAllTimers();
+
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledWith(args);
+    });
+});
