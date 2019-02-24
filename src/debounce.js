c9040c7dcc2938691bd93608eec5135a1588dd32
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-02-24 11:54:57 +0000

    remove unnecessary files

--- src/debounce.js
+++
@@ -1,9 +0,0 @@
-export default (callback, wait) => {
-    let timeout;
-
-    return (...args) => {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => callback.apply(context, args), wait);
-    }
-}
